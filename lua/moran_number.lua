4434e976eb7ceacb38f850131d524c74dc555097
ksqsf <ksqsf@users.noreply.github.com>
2025-12-18 20:17:12 +0000

    deploy: 9a4ea931347cefae57aec73c5d04a0a731ef9ff3

--- lua/moran_number.lua
+++ lua/moran_number.lua
@@ -10,27 +10,27 @@
 -- along with build & install instructions.
 --
 -- ChangeLog:
--- 0.1.1: 把「小写」改为日常一般读法。
+-- 0.1.1: 把「小寫」改爲日常一般讀法。
 -- 0.1: Introduction.
 
-local dot              = "点"
+local dot              = "點"
 local digitRegular     = { [0] = "零", "一", "二", "三", "四", "五", "六", "七", "八", "九" }
 local digitLower       = { [0] = "〇", "一", "二", "三", "四", "五", "六", "七", "八", "九" }
-local digitUpper       = { [0] = "零", "壹", "贰", "叁", "肆", "伍", "陆", "柒", "捌", "玖" }
+local digitUpper       = { [0] = "零", "壹", "貳", "叄", "肆", "伍", "陸", "柒", "捌", "玖" }
 local unitLower        = { "", "十", "百", "千" }
 local unitUpper        = { "", "拾", "佰", "仟" }
-local bigUnit          = { "万", "亿" }
+local bigUnit          = { "萬", "億" }
 local currencyUnit     = "元"
-local currencyFracUnit = { "角", "分", "厘", "毫" }
+local currencyFracUnit = { "角", "分", "釐", "毫" }
 
--- 解析浮点数字符串为三元组 ( 整数部分字符串, 小数点字符串, 小数部分字符串 )
+-- 解析浮點數字符串爲三元組 ( 整數部分字符串, 小數點字符串, 小數部分字符串 )
 local function parseNumStr(str)
    local result = {}
    result.int, result.dot, result.frac = str:match("^(%d*)(%.?)(%d*)")
    return result
 end
 
--- 转换 4 位整数节, 如 9909 -> 九千九百零九
+-- 轉換 4 位整數節, 如 9909 -> 九千九百零九
 local function translateIntSegment(int, digit, unit)
    local d = {
       int % 10,
@@ -46,7 +46,7 @@ local function translateIntSegment(int, digit, unit)
          if lastPos == -1 then
             lastPos = i
          end
-         if lastPos - i > 1 then  -- 中间有空位, 增加'零'
+         if lastPos - i > 1 then  -- 中間有空位, 增加'零'
             result = result .. digit[0]
          end
          result = result .. digit[d[i]] .. unit[i]
@@ -57,12 +57,12 @@ local function translateIntSegment(int, digit, unit)
    return result
 end
 
--- 将指数转换成大数单位
--- 如 4->万, 8->亿
--- exponent 必须是4的倍数
+-- 將指數轉換成大數單位
+-- 如 4->萬, 8->億
+-- exponent 必須是4的倍數
 local function translateBigUnit(exponent, bigUnit)
    exponent = exponent // 4
-   local hiExp = #bigUnit    -- 最高大数单位
+   local hiExp = #bigUnit    -- 最高大數單位
    local result = bigUnit[hiExp]:rep(exponent // hiExp)
    exponent = exponent % hiExp
    local i = 1
@@ -77,11 +77,11 @@ local function translateBigUnit(exponent, bigUnit)
    return prefix .. result
 end
 
--- 转换整数部分
+-- 轉換整數部分
 local function translateInt(str, digit, unit, bigUnit)
    local int = tonumber(str)
    if math.type(int) == "float" then
-      return "数值超限！"
+      return "數值超限！"
    end
    if int == 0 then
       return digit[0]
@@ -110,7 +110,7 @@ local function mapDigits(str, digit)
    return str:gsub("%d", function(c) return digit[tonumber(c)] or c end)
 end
 
--- 转换小数部分, 金额风格, 0123 -> 零角一分二厘
+-- 轉換小數部分, 金額風格, 0123 -> 零角一分二釐
 local function translateFracCurrency(str, digit, unit)
    local len = math.min(#unit, #str)
    local result = ""
@@ -121,8 +121,8 @@ local function translateFracCurrency(str, digit, unit)
    return result .. terminator
 end
 
--- 常规转换
--- 日常读法中最开始的「一十」省略「十」
+-- 常規轉換
+-- 日常讀法中最開始的「一十」省略「十」
 local function translateRegular(input)
    local res = translateInt(input.int, digitRegular, unitLower, bigUnit)
       .. (input.dot ~= "" and (dot .. mapDigits(input.frac, digitRegular)) or "")
@@ -139,7 +139,7 @@ local function translateLower(input)
       .. (input.dot ~= "" and (dot .. mapDigits(input.frac, digitLower)) or "")
 end
 
--- 金额转换
+-- 金額轉換
 local function translateCurrency(input, digit, unit, bigUnit)
    local intPart = translateInt(input.int, digit, unit, bigUnit)
    local fracPart = translateFracCurrency(input.frac or "", digit, currencyFracUnit)
@@ -149,11 +149,11 @@ end
 local function translateNumStr(str)
    local input = parseNumStr(str)
    local result = {
-      { "〔常规〕", translateRegular(input), },
-      { "〔编号〕", mapDigits(str, digitLower):gsub("%.", dot) },
-      { "〔大写〕", translateUpper(input) },
-      { "〔金额大写〕", translateCurrency(input, digitUpper, unitUpper, bigUnit) },
-      { "〔金额小写〕", translateCurrency(input, digitLower, unitLower, bigUnit) },
+      { "〔常規〕", translateRegular(input), },
+      { "〔編號〕", mapDigits(str, digitLower):gsub("%.", dot) },
+      { "〔大寫〕", translateUpper(input) },
+      { "〔金額大寫〕", translateCurrency(input, digitUpper, unitUpper, bigUnit) },
+      { "〔金額小寫〕", translateCurrency(input, digitLower, unitLower, bigUnit) },
    }
    return result
 end
