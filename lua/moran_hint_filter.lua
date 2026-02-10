a1e7020372c8471e690be11718425a9f368e7b15
ksqsf <ksqsf@users.noreply.github.com>
2026-02-10 19:38:51 +0000

    deploy: 54bccae605cd3ffbee6ea49fe15ff37bdc0bf531

--- lua/moran_hint_filter.lua
+++ lua/moran_hint_filter.lua
@@ -1,10 +1,12 @@
 -- Moran Translator (for Express Editor)
 -- Copyright (c) 2023-2026 ksqsf
 --
--- Ver: 0.4.0
+-- Ver: 0.4.1
 --
 -- This file is part of Project Moran
 -- Licensed under GPLv3
+--
+-- 0.4.1: 修復 aux_table 格式未適配問題。
 --
 -- 0.4.0: 適配多字詞重排。
 --
