42c8b89af3254ebd6f6a5076b33ee2682eb96c3f
ksqsf <ksqsf@users.noreply.github.com>
2026-05-05 13:16:59 +0000

    deploy: bbd2a75dfc37687e113f1f4eb7c53d8e42fd9ac8

--- lua/moran_ijrq_filter.lua
+++ lua/moran_ijrq_filter.lua
@@ -81,7 +81,11 @@ function Module.func(t_input, env)
         yield(first_cand)
         moran.yield_all(iter)
     elseif input_len == 5 or input_len == 7 or input_len == 9 then
-        if input:sub(1,input_len-1) ~= env.last_input or utf8.len(env.last_first_cand) == 1 then
+        local iter = moran.make_peekable(iter)
+        local first = iter:peek()
+        local last_len = utf8.len(env.last_first_cand)
+        local first_len = utf8.len(first.text)
+        if input:sub(1,input_len-1) ~= env.last_input or last_len == 1 or last_len ~= first_len then
             moran.yield_all(iter)
             return
         end
@@ -89,14 +93,6 @@ function Module.func(t_input, env)
         -- If the user types auxcode super fast, then we should NOT
         -- attempt to postpone first cand.
         if not Module.debounce(env) then
-            moran.yield_all(iter)
-            return
-        end
-
-        -- check if first is a completion (#219)
-        local iter = moran.make_peekable(iter)
-        local first = iter:peek()
-        if first and utf8.len(first.text) ~= utf8.len(env.last_first_cand) then
             moran.yield_all(iter)
             return
         end
