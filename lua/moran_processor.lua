b9c4b049595cdfeaac7d26382650bab1f44b78af
ksqsf <ksqsf@users.noreply.github.com>
2026-06-04 18:22:53 +0000

    deploy: e6b7e310f29ac9e25e462e61e7f4e51429736dbe

--- lua/moran_processor.lua
+++ lua/moran_processor.lua
@@ -298,19 +298,30 @@ return {
         end
 
         -- 用戶啓用的字集
-        variants = cfg:get_list("switches/@2/options")
-        if variants.size >= 2
-            and variants:get_at(0).type == "kScalar"
-            and variants:get_at(0):get_value():get_string():match("^std_")
-            and variants:get_at(1).type == "kScalar"
-            and variants:get_at(1):get_value():get_string():match("^std_")
-        then
-            env.variants = {
-                variants:get_at(0):get_value():get_string(),
-                variants:get_at(1):get_value():get_string(),
-            }
-        else
-            log.error('moran_processor: switches/@2 不是字形變體列表, 如 "std_t2s"! 回落至默認值 [std_t, std_t2s]')
+        local switches = cfg:get_list("switches")
+        if switches then
+            for i = 0, switches.size - 1 do
+                local switch = switches:get_at(i)
+                local switch_map = switch and switch:get_map()
+                local options = switch_map and switch_map:get("options")
+                local variants = options and options:get_list()
+                if variants
+                    and variants.size >= 2
+                    and variants:get_at(0).type == "kScalar"
+                    and variants:get_at(0):get_value():get_string():match("^std_")
+                    and variants:get_at(1).type == "kScalar"
+                    and variants:get_at(1):get_value():get_string():match("^std_")
+                then
+                    env.variants = {
+                        variants:get_at(0):get_value():get_string(),
+                        variants:get_at(1):get_value():get_string(),
+                    }
+                    break
+                end
+            end
+        end
+        if not env.variants then
+            log.error('moran_processor: switches 中未找到字形變體列表, 如 "std_t2s"! 回落至默認值 [std_t, std_t2s]')
             env.variants = { "std_t", "std_t2s" }
         end
         -- print('v1 = ' .. env.variants[1])
